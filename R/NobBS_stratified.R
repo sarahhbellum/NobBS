3ed07b5c8aab2b37372365ffc5978c911ea0c11a
Sarah McGough <sarahhbellum@users.noreply.github.com>
2023-11-08 16:17:21 -0800

    Fix tidyverse deprecations in stratified

--- R/NobBS_stratified.R
+++ R/NobBS_stratified.R
@@ -28,7 +28,6 @@
 #' @import rjags
 #' @importFrom dplyr last
 #' @importFrom dplyr select
-#' @importFrom tidyselect vars_select
 #' @importFrom dplyr starts_with
 #' @importFrom dplyr group_by
 #' @importFrom dplyr n
@@ -288,41 +287,41 @@ NobBS.strat <- function(data, now, units, onset_date, report_date, strata, movin
   # Combine nowcast estimates with: dates, number of cases reported at each date and strata
   reported <- data.frame(
     realtime.data %>%
-      group_by(onset_date,strata) %>%
-      summarise(n.reported=n())
+      dplyr::group_by(!!sym(onset_date),!!sym(strata)) %>%
+      dplyr::summarise(n.reported=dplyr::n())
   )
   names(reported)[1] <- "onset_date"
   names(reported)[2] <- "stratum"
 
   estimates <- data.frame(estimates, onset_date=rep(seq(as.Date(now)-w.days,as.Date(now),by=units),times=S)) %>%
-    left_join(reported,by=c("onset_date","stratum"))
+    dplyr::left_join(reported,by=c("onset_date","stratum"))
 
   estimates.inflated <- data.frame(estimates.inflated, onset_date=rep(seq(as.Date(now)-w.days,as.Date(now),by=units),times=S)) %>%
-    left_join(reported,by=c("onset_date","stratum"))
+    dplyr::left_join(reported,by=c("onset_date","stratum"))
 
   t <- now.T
 
   parameter_extract <- matrix(NA, nrow=10000)
 
   if("lambda"%in%specs$param_names){
-    parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(vars_select(names(mymod.dat),starts_with(paste("lambda[",t,",",sep="")))))
+    parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(starts_with(paste("lambda[",t,",",sep=""))))
   }
   if("beta.logged"%in%specs$param_names){
     betas.logged<- matrix(NA,nrow=10000,ncol=(max_D+1))
     dimnames(betas.logged) = list(NULL,c(paste("Beta",c(0:max_D))))
     for(d in 0:max_D){
-      betas.logged[,(d+1)] <- (mymod.dat %>% dplyr::select(vars_select(names(mymod.dat),starts_with(paste("beta.logged[",(d+1),"]",sep="")))))[,1]
+      betas.logged[,(d+1)] <- (mymod.dat %>% dplyr::select(starts_with(paste("beta.logged[",(d+1),"]",sep=""))))[,1]
     }
     parameter_extract <- cbind(parameter_extract,betas.logged)
   }
   if("alpha"%in%specs$param_names){
-    parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(vars_select(names(mymod.dat),starts_with(paste("alpha[",t,sep="")))))
+    parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(starts_with(paste("alpha[",t,sep=""))))
   }
   if("tau2.alpha"%in%specs$param_names){
-    parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(vars_select(names(mymod.dat),starts_with("tau2.alpha"))))
+    parameter_extract <- cbind(parameter_extract,mymod.dat %>% dplyr::select(starts_with("tau2.alpha")))
   }
 
-  nowcast.post.samps <- (mymod.dat %>% dplyr::select(vars_select(names(mymod.dat),starts_with(paste("sum.n[",t,sep="")))))[,1]
+  nowcast.post.samps <- (mymod.dat %>% dplyr::select(starts_with(paste("sum.n[",t,sep=""))))[,1]
 
   list(estimates=estimates,estimates.inflated=estimates.inflated, nowcast.post.samps=nowcast.post.samps,params.post=parameter_extract[,2:ncol(parameter_extract)])
 
